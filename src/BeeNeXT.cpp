76492a502dd96bbe7736e59c5b25911ac7a24941
BlynkGO <blynkgo@gmail.com>
2022-12-16 10:17:49 +0700

    V1.0.1
    SERIAL_DATA เพิ่ม

--- src/BeeNeXT.cpp
+++ src/BeeNeXT.cpp
@@ -4,6 +4,7 @@
 
 void BeeNeXT_NoOpCbk() {}
 BEENEXT_DATA()    __attribute__((weak, alias("BeeNeXT_NoOpCbk")));
+SERIAL_DATA()    	__attribute__((weak, alias("BeeNeXT_NoOpCbk")));
 
 BeeNeXT_class BeeNeXT;
 
@@ -13,8 +14,16 @@ BeeNeXT_class::~BeeNeXT_class(){
 
 void BeeNeXT_class::begin(HardwareSerial *serial){
   this->end();
-
+#if CONFIG_IDF_TARGET_ESP32S3
+#if defined(BEENEXT_4_3) ||  defined(BEENEXT_4_3C) ||  defined(BEENEXT_4_3IPS)
+  pinMode(18,OUTPUT);
+  digitalWrite(18, LOW); // ทำขา 18 เป็น GND
+#endif
+  Serial2.begin(115200);  // RX19 ; TX20
+  _hw_serial = &Serial2;
+#else
   _hw_serial = (serial == NULL)? &Serial : serial; 
+#endif
   _hw_serial->setTimeout(50);
 }
 
@@ -22,7 +31,7 @@ void BeeNeXT_class::begin(HardwareSerial &serial ){
   this->begin(&serial);
 }
 
-#if BEENEXT_USE_SOFTWARESERIAL
+#if BEENEXT_USE_SOFTWARESERIAL && (CONFIG_IDF_TARGET_ESP32S3==0)
 void BeeNeXT_class::begin(unsigned long baud, uint8_t rx, uint8_t tx){
   this->end();
   _sw_serial = new SoftwareSerial();
@@ -37,13 +46,24 @@ void BeeNeXT_class::begin(uint8_t rx, uint8_t tx){
 }
 #endif //#if BEENEXT_USE_SOFTWARESERIAL
 
+// #if defined(CONFIG_IDF_TARGET_ESP32S3)
+// void BeeNeXT_class::begin(unsigned long baud, uint8_t rx, uint8_t tx){
+//   Serial.println("[BeeNeXT] use Serial2");
+//   Serial2.begin(baud, SERIAL_8N1, rx, tx);
+//   this->begin(&Serial2);
+// }
+
+// void BeeNeXT_class::begin(uint8_t rx, uint8_t tx){
+//   this->begin(115200, rx, tx);
+// }
+// #endif // #if defined(ESP32)
 
 void BeeNeXT_class::end(){
   if( _hw_serial != NULL) { 
     _hw_serial->end(); 
     _hw_serial = NULL;
   }
-#if BEENEXT_USE_SOFTWARESERIAL
+#if BEENEXT_USE_SOFTWARESERIAL && (CONFIG_IDF_TARGET_ESP32S3==0)
   if( _sw_serial != NULL ){
     _sw_serial->end();
     if(_is_swserial_alloced){
@@ -62,10 +82,12 @@ void BeeNeXT_class::update(){
         data.replace("[BN]", "");
         _data = data;
         BeeNeXT_onData();
+      }else{
+        BeeNeXT_onSerialData();
       }
     }
   }
-#if BEENEXT_USE_SOFTWARESERIAL
+#if BEENEXT_USE_SOFTWARESERIAL && (CONFIG_IDF_TARGET_ESP32S3==0)
   else if(_sw_serial !=NULL ){
     if(_sw_serial->available()){
       String data = _sw_serial->readStringUntil('\n'); _data.trim();
@@ -73,6 +95,8 @@ void BeeNeXT_class::update(){
         data.replace("[BN]", "");
         _data = data;
         BeeNeXT_onData();
+      }else{
+        BeeNeXT_onSerialData();
       }
     }
   }
@@ -92,21 +116,19 @@ void BeeNeXT_class::command(uint16_t cmd) {
   }
 }
 
-
-
-
+//------------------------------------------------------------
 //virtual function
 size_t BeeNeXT_class::write(uint8_t data){
   if(_hw_serial){
     _hw_serial->write('[');_hw_serial->write('B');_hw_serial->write('N');_hw_serial->write(']');
-    _hw_serial->write(data);
-//    _hw_serial->write('\n');
+    size_t sz = _hw_serial->write(data);
+    return sz+4;
   }
-#if BEENEXT_USE_SOFTWARESERIAL
+#if BEENEXT_USE_SOFTWARESERIAL && (CONFIG_IDF_TARGET_ESP32S3==0)
   else if( _sw_serial){
     _sw_serial->write('[');_sw_serial->write('B');_sw_serial->write('N');_sw_serial->write(']');
-    _sw_serial->write(data);
-//    _sw_serial->write('\n');
+    size_t sz = _sw_serial->write(data);
+    return sz+4;
   }
 #endif
 }
@@ -114,14 +136,14 @@ size_t BeeNeXT_class::write(uint8_t data){
 size_t BeeNeXT_class::write(const uint8_t *buffer, size_t size){
   if(_hw_serial){
     _hw_serial->write('[');_hw_serial->write('B');_hw_serial->write('N');_hw_serial->write(']');
-    _hw_serial->write(buffer, size);
-//    _hw_serial->write('\n');
+    size_t sz = _hw_serial->write(buffer, size);
+    return sz + 4;
   }
-#if BEENEXT_USE_SOFTWARESERIAL
+#if BEENEXT_USE_SOFTWARESERIAL && (CONFIG_IDF_TARGET_ESP32S3==0)
   else if(_sw_serial){
     _sw_serial->write('[');_sw_serial->write('B');_sw_serial->write('N');_sw_serial->write(']');
-    _sw_serial->write(buffer, size);
-//    _sw_serial->write('\n');
+    size_t sz = _sw_serial->write(buffer, size);
+    return sz + 4;
   }
 #endif
 }
--- src/BeeNeXT.h
+++ src/BeeNeXT.h
@@ -1,7 +1,7 @@
 #ifndef __BEENEXT_H__
 #define __BEENEXT_H__
 
-#define BEENEXT_VERSION     1.0.0
+#define BEENEXT_VERSION     1.0.1
 
 //#include "../../config/blynkgo_config.h"
 #define BEENEXT
@@ -22,17 +22,18 @@
   #include "libs/BeeNeXT_SoftTimer/BeeNeXT_SoftTimer.h"
 #endif
 
-#if BEENEXT_USE_SOFTWARESERIAL
+#if BEENEXT_USE_SOFTWARESERIAL && (CONFIG_IDF_TARGET_ESP32S3==0)
   #include "libs/SoftwareSerial/SoftwareSerial.h"
 #endif
 
-#define BEENEXT_DATA()  void BeeNeXT_onData()
-
+#define BEENEXT_DATA()      void BeeNeXT_onData()
+#define SERIAL_DATA()       void BeeNeXT_onSerialData()
 #ifdef __cplusplus
 extern "C" {
 #endif
   void BeeNeXT_NoOpCbk();
   BEENEXT_DATA();
+  SERIAL_DATA();
 #ifdef __cplusplus
 }
 #endif
@@ -48,11 +49,16 @@ class BeeNeXT_class : public Print {
     void begin(HardwareSerial *serial=NULL );  
     void begin(HardwareSerial &serial );
     
-#if BEENEXT_USE_SOFTWARESERIAL
+#if BEENEXT_USE_SOFTWARESERIAL && (CONFIG_IDF_TARGET_ESP32S3==0)
     // API begin(..) นี้ จะใช้ SW Serial
     void begin(unsigned long baud, uint8_t rx, uint8_t tx);
     void begin(uint8_t rx, uint8_t tx);
 #endif
+// #if CONFIG_IDF_TARGET_ESP32S3
+//     void begin(unsigned long baud, uint8_t rx, uint8_t tx);
+//     void begin(uint8_t rx, uint8_t tx);
+// #endif
+
     void end();
 
     void update();
@@ -72,7 +78,7 @@ class BeeNeXT_class : public Print {
   private:
     String _data;
     HardwareSerial * _hw_serial=NULL;
-#if BEENEXT_USE_SOFTWARESERIAL
+#if BEENEXT_USE_SOFTWARESERIAL && (CONFIG_IDF_TARGET_ESP32S3==0)
     SoftwareSerial * _sw_serial=NULL;
     bool  _is_swserial_alloced=false;
 #endif
