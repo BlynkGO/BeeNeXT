4a6c6bcc6e46e9a4a356b5cab84cefb4325d12a9
BlynkGO <65327357+BlynkGO@users.noreply.github.com>
2023-03-13 02:04:15 +0700

    add example

---
+++ examples/03.TwoMCUs_communication/ArduinoUNO_code/ArduinoUNO_code.ino
@@ -0,0 +1,38 @@
+#include <BeeNeXT.h>
+
+#define LED_PIN    13
+
+void setup() {
+  Serial.begin(115200); Serial.println();
+  Serial.println("BeeNeXT Test");
+
+  // --- แบบใช้ Software Serial ----------
+  BeeNeXT.begin( 6 ,  7 );  // BeeNeXT เริ่มทำงานด้วย Soft Serial ขา RX6, TX7
+
+
+  pinMode(LED_PIN,OUTPUT);
+
+  static SoftTimer timer;
+  timer.setInterval(1000,[](){
+    BeeNeXT.send("TEMP" , random(100.0)/10.0 );    // ตัวอย่างส่งแบบ key & value จาก MCU ปัจจุบันไปอีก MCU
+    BeeNeXT.send("HUMID", random(100.0)/10.0 );    // ตัวอย่างส่งแบบ key & value จาก MCU ปัจจุบันไปอีก MCU
+  });
+
+}
+
+void loop() {
+  BeeNeXT.update();
+}
+
+// ตัวอย่าง รับค่า value ที่ส่งมาจาก MCU อื่น ด้วย key "LED" 
+// แล้วนำค่า value มากำหนดให้ led ที่ต่อกับ MCU นี้
+
+BEENEXT_DATA(){
+  if(BeeNeXT.key() == "LED") {                      // เช็คว่า key ที่ส่งมาตรงกับ "LED" หรือไม่
+    bool value = (bool) BeeNeXT.value().toInt();    // เปลี่ยนให้ value ที่ส่งมา ไปเป็นข้อมูลแบบ boolean  ; อาจจะเปลี่ยนไปค่าแบบต่างๆได้เช่น BeeNeXT.toString(), BeeNeXT.toInt(), BeeNeXT.toFloat(), BeeNeXT.toBool()  ได้ตามต้องการ
+    digitalWrite(LED_PIN, value); 
+  }
+}
+
+
+
