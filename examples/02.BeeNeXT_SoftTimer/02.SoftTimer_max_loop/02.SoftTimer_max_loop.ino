ff21da9cc43a8fa72204965213213f88a2fc8db9
BlynkGO <65327357+BlynkGO@users.noreply.github.com>
2024-03-04 19:29:21 +0700

    เพิ่มตัวอย่าง

---
+++ examples/02.BeeNeXT_SoftTimer/02.SoftTimer_max_loop/02.SoftTimer_max_loop.ino
@@ -0,0 +1,25 @@
+#include <BeeNeXT.h>
+
+#define LED_PIN    13
+
+void setup() {
+  Serial.begin(115200); Serial.println();
+
+  pinMode(LED_PIN,OUTPUT);
+
+  static SoftTimer timer;
+  timer.setInterval(500,10,[](){             // ตั้งเวลาทำงานเป็นช่วงๆ แบบนี้จะไม่ถูก blocking code เป็นจำนวน 10 ครั้ง
+    digitalWrite(LED_PIN, !digitalRead(LED_PIN));
+  });
+  timer.ready_cb([](){
+    digitalWrite(LED_PIN,LOW);
+    Serial.println("[Timer] done!");
+  );
+
+}
+
+void loop() {
+  BeeNeXT.update();
+}
+
+
